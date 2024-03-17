293da3df1be63dc619f8df6908a6d6bbd1df2098
commondrum <karol.hetman@protonmail.com>
2024-03-17 13:59:15 +0000

    Progres

--- game.pl
+++ game.pl
@@ -1,57 +1,31 @@
-location(cellar).
-path(cellar, beachWest).
-
-describe(cellar) :-
+description(cellar) :-
     !visited(cellar),
     write('You are in the cellar. It is dark and damp. You can see a it is full of old barrels and crates.'),
     assert(visited(cellar)), nl.
 
-describe(cellar) :-
-    visited(cellar),
-    write('Weeee you visited the cellar'), nl.
+desctiption(cellar) :-
+    write('You are in the cellar. It is dark and damp. You can see a it is full of old barrels and crates.'), nl.
 
 
 location(beachNorth).
 location(beachSouth).
-location(beachEast).
-location(beachWest).
-location(jungleEast).
-location(jungleWest).
+location(jungleSouth).
+location(jungleNorth).
+location(cellar).
 
+path(beachSouth , beachNorth).
+path(beachSouth , jungleSouth).
+path(jungleSouth , jungleNorth).
+path(beachNorth , jungleNorth).
+path(cellar, beachNorth).
+path(X,Y) :- path(Y,X).
 
-
-path(beachWest, cellar).
-path(beachWest, beachSouth).
-path(beachWest, beachNorth).
-path(beachWest, jungleWest).
-
-path(beachSouth, beachWest).
-path(beachSouth, beachEast).
-path(beachSouth, jungleEast).
-path(beachSouth, jungleWest).
-
-path(beachEast, beachSouth).
-path(beachEast, jungleEast).
-path(beachEast, beachNorth).
-
-path(beachNorth, beachWest).
-path(beachNorth, beachEast).
-path(beachNorth, jungleEast).
-path(beachNorth, jungleWest).
-
-path(jungleEast, beachSouth).
-path(jungleEast, beachEast).
-path(jungleEast, beachNorth).
-path(jungleEast, jungleWest).
-
-
-
-move(Direction) :-
-    current_location(CurrentLocation),
-    path(CurrentLocation, Direction, NextLocation),
-    retract(current_location(CurrentLocation)),
-    asserta(current_location(NextLocation)),
-    describe(NextLocation).
+move(location) :-
+    current_location(Here),
+    path(Here, There),
+    retract(current_location(Here)),
+    assert(current_location(There)),
+    describe(There).
 
 move(_) :-
     write('You can''t go that way.'), nl.
@@ -60,14 +34,6 @@ look :-
     current_location(Location),
     describe(Location).
 
-command(move(Direction)) :- move(Direction).
-command(look) :- look.
 
-:- dynamic current_location/1.
 start :-
-    retractall(current_location(_)),
-    asserta(current_location(cellar)),
-    write('Welcome to the Island Adventure!'), nl,
-    write('Type command(Action) to perform actions, e.g., command(move(north)).'), nl,
-    describe(cellar).
 

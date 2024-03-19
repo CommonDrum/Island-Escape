2fc56a815b5e4c7f66492f7fde99b27a1ad92c9a
Karol Hetman <karol.hetman@protonmail.com>
2024-03-19 15:56:39 +0000

    Obejcts and items

--- items.pl
+++ items.pl
@@ -7,3 +7,9 @@ item(glassBottle, beachSouth, 'A glass bottle is lying on the sand.', 'The glass
 item(flintStone, _, 'You spot a small flint stone.', 'The flint stone has sharp edges and could be used to start a fire.').
 item(oldBarrels, cellar, 'There are some old barrels in the cellar.', 'The barrels are empty and covered in cobwebs.').
 item(oldCrates, cellar, 'You notice a few old crates in the corner.', 'The crates contain some ancient artifacts.').
+item(cocoNut, _, 'Coconuts are hanging from the trees.', 'The coconuts are ripe and can be eaten. But you need to find a way to open them.').
+item(hardStick, _, 'You see a hard stick on the ground.', 'The stick is sturdy and may be useful for crafting.').
+item(bigLeafs, _, 'There are many bushes with big leafs.', 'The leafs are big and can be used to make a shelter.').
+item(bottleOfUnpurifiedWater, _, _, 'The water is not safe to drink. You need to purify it first.').
+item(bottleOfPurifiedWater, _, _, 'The water is safe to drink.').
+item(textileSheet, beachSouth, 'A big textile sheet is stuck on a branch. It looks like ripped sail.', 'The sheet is big and not damaged. It sure will be useful for something.').
--- objects.pl
+++ objects.pl
@@ -1,4 +1,8 @@
 % objects.pl
 
 object(largeRock, jungleSouth, 'There is a large rock nearby.', 'You come closer to the rock and discover a bunch of flintstones laying around it.', [flintStone, flintStone]).
-object(oldTree, jungleNorth, 'An old tree stands tall in the jungle.', 'The tree has some peculiar carvings on its trunk.', []).
+Object(waterPool, jungleSouth, 'You see a small pool of water.', 'Water! You can drink it, but it is not safe to drink. You need to purify it first.', _).
+object(palmTrees, jungleNorth, 'You see a few palm trees.', 'The palm trees are tall and have coconuts hanging from them.', [cocoNut]).
+Object(cave, beachNorth, 'There is a small cave-like structure in the rocks.', 'It looks like a good place to stay, but it lacks come warmth...', _).
+object(bush, jungleSouth, 'You see a bush with big leafs.', 'You strip the bush out of its leafs and gather some sticks.', [bigLeafs, hardStick]).
+object(bay, beachNorth, 'Small bay gliters in the sun.', 'The water is calm and clear. You think it that if you had a boat, you could sail away from here.', _).

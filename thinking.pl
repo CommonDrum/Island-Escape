879617f74d6e63b7777662a7ec3f74e55709b439
Karol Hetman <karol.hetman@protonmail.com>
2024-03-25 12:36:07 +0100

    123

--- thinking.pl
+++ thinking.pl
@@ -10,33 +10,15 @@ think :-
     writeln(Thought),
     add_goals(Goals),
     retract(to_think(Thought, Goals)),
-<<<<<<< HEAD
-    fail.
-
-think :- 
-    findall(D, goal(_, D, _), L),
-    write(L), nl.
-
-
-fulill_goal(Goal) :-
-    goal(Goal, Description, Value),
-    writeln('You have fulfilled a goal!'),
-    writeln(Description),
-    steps_remaining(X),
-    Y is X + Value,
-    retract(steps_remaining(X)),
-    assertz(steps_remaining(Y)),
-    retract(goal(Goal, Description, Value)).
-
-fulill_goal.
-=======
     logic,
     !.
->>>>>>> 9a4acf7965ff74c8c3d568251fc55949ea05450e
 
 add_goals([]).
 add_goals([goal(ID, Desc, Time) | Rest]) :-
-    assertz(goal(ID, Desc, Time))
+    (   goal(ID, _, _) -> 
+        true ; 
+        assertz(goal(ID, Desc, Time))
+    ),
     add_goals(Rest).
 
 see_goals :-
